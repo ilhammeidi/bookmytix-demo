48293a3dc537089952658e0a0057932d5b201479
retinam2 <ilham.meidi@gmail.com>
2025-05-01 09:22:22 +0700

    first commit

---
+++ pages/[locale]/index.js
@@ -0,0 +1,207 @@
+import React, { useState, Fragment } from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Head from 'next/head';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import AndroidIcon from '@mui/icons-material/Android';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import PersonIcon from '@mui/icons-material/Person';
+import Dialog from '@mui/material/Dialog';
+import { TransitionProps } from '@mui/material/transitions';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import HeaderBasic from '~/components/Header/Basic';
+import { getStaticPaths, makeStaticProps } from '~/lib/getStatic';
+import Logo from '~/components/Logo';
+import { useTextAlign, useText } from '~/theme/common';
+import useStyles from '~/theme/mobile-demo-style';
+import brand from '~/public/text/brand';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const productLink = 'https://codecanyon.net/item/bookmytix-flutter-flight-booking-app-template/57827751';
+
+function MoreAbout() {
+  const { classes } = useStyles();
+  
+  return <Fragment>
+    <Box sx={{py: 3}}>
+      <Logo type="text" />
+    </Box>
+    <Typography align="left">
+      BookMyTix web version demo. This template built with <strong>Flutter</strong> and support for <strong>Android, iOS, and Mobile Web.</strong>
+      You can get the Android Installer with APK file from link bellow.
+    </Typography>
+    <Box sx={{ py: 3 }}>
+      <Button fullWidth size="large" target="_blank" href={productLink} variant="contained" color="primary">BUY NOW</Button>
+    </Box>
+    <Grid container spacing={1} className={classes.btnCta}>
+      <Grid item xs={6}>
+        <Button size="small" fullWidth variant="outlined" color="black">
+          <AndroidIcon sx={{ mr: 0.5 }} /> APK Installer
+        </Button>
+      </Grid>
+      <Grid item xs={6}>
+        <Button size="small" fullWidth variant="outlined" color="black">
+          <GitHubIcon sx={{ mr: 0.5 }} /> Try Free Version
+        </Button>
+      </Grid>
+    </Grid>
+    <br />
+    <Grid container spacing={1} className={classes.btnCta}>
+      <Grid item xs={6}>
+        <Button size="small" fullWidth variant="outlined" color="black" target="_blank" href="https://ilhammeidi.github.io/bookmytix-docs">
+          <MenuBookIcon sx={{ mr: 0.5 }} /> Online Docs
+        </Button>
+      </Grid>
+      <Grid item xs={6}>
+        <Button size="small" fullWidth variant="outlined" color="black" target="_blank" href="https://themeforest.net/user/ilhammeidi">
+          <PersonIcon sx={{ mr: 0.5 }} /> Contact Author
+        </Button>
+      </Grid>
+    </Grid>
+    <br />
+    <Typography>
+      If you need installer for iOS and installation guide, please contact the author:
+      <br />
+      <a target="_blank" href="https://themeforest.net/user/ilhammeidi">https://themeforest.net/user/ilhammeidi</a>
+    </Typography>
+  </Fragment>
+}
+
+function LandingPage() {
+  const { classes, cx } = useStyles();
+  const { classes: align } = useTextAlign();
+  const { classes: text } = useText();
+
+  // Breakpoints
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
+  
+  // Zoom
+  const [zoom, setZoom] = useState('medium');
+  const handleZoom = (val) => {
+    setZoom(val);
+  }
+  
+  // Mobiel Option
+  const [open, setOpen] = React.useState(false);
+  
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
+  return (
+    <Fragment>
+      <Head>
+        <title>
+          { brand.agency.name + ' - Web Demo' }
+        </title>
+      </Head>
+      <CssBaseline />
+      {!isDesktop && (
+        <div className={classes.header}>
+          <HeaderBasic text="Buy Now" href={productLink} handleShowOption={handleClickOpen} />
+        </div>
+      )}
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              More About BookMyTix
+            </Typography>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Box p={2}>
+          <MoreAbout />
+        </Box>
+      </Dialog>
+      <div className={classes.fullScreenContainer}>
+        <Grid container spacing={2} justifyContent="center">
+          {isDesktop && (
+            <Grid item lg={3} xs={12}>
+              <MoreAbout />
+            </Grid>
+          )}
+
+          <Grid item lg={5} xs={12}>
+            <div className={cx(classes.phoneWrap, zoom == 'small' && classes.noscroll)}>
+              <div className={cx(classes.phoneFrame, classes[zoom])}>
+                <div className={classes.phoneContent}>
+                  <iframe  width="100%" height="100%" src="https://bookmytixweb.vercel.app/"></iframe>
+                </div>
+              </div>
+            </div>
+          </Grid>
+
+          {isDesktop && (
+            <Grid item lg={3} xs={12}>
+              <div className={classes.optZoom}>
+                <h3 className={text.subtitle}>
+                  Zoom Options
+                </h3>
+                <ButtonGroup size="large" aria-label="Large button group">
+                  <Button onClick={() => handleZoom('small')} variant={zoom == 'small' ? 'contained' : 'outlined'}>Small (50%)</Button>
+                  <Button onClick={() => handleZoom('medium')} variant={zoom == 'medium' ? 'contained' : 'outlined'}>Medium (75%)</Button>
+                  <Button onClick={() => handleZoom('large')} variant={zoom == 'large' ? 'contained' : 'outlined'}>Large (100%)</Button>
+                </ButtonGroup>
+                <br />
+                <Button
+                  fullWidth
+                  href="https://bookmytixweb.vercel.app/"
+                  target="_blank"
+                  size="large"
+                  variant="contained"
+                  color="black"
+                >
+                  <FullscreenIcon />
+                  &nbsp;
+                  Show Full Screen
+                </Button>
+              </div>
+            </Grid>
+          )}
+        </Grid>
+      </div>
+    </Fragment>
+  );
+}
+
+export default LandingPage;
+
+// Use this below for Server Side Render/Translation (SSR)
+// export const getStaticProps = async ({ locale }) => ({ props: { ...await serverSideTranslations(locale, ['common']) } });
+
+// Use this below for Static Site Generation (SSG)
+const getStaticProps = makeStaticProps(['common']);
+export { getStaticPaths, getStaticProps };
